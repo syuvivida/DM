9be99fa7459669d9d53e963a5fc91ddf598b124d
Shin-Shan Yu <syu@ncu200.cern.ch>
2016-10-03 13:49:30 +0200

    cross sections and launch scripts for baryonic Z'

---
+++ launch_scripts/getCrossSection.py
@@ -0,0 +1,21 @@
+import os
+import sys
+print sys.argv
+
+name=sys.argv[1]
+filename='crossSection_'+name+'.txt'
+os.system('rm -rf '+filename)
+
+dmrange =[1,10,20,50,100,150,500,1000]
+medrange=[10,20,50,100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,2000,10000]
+for med    in medrange:
+    for dm in dmrange:
+        tmpMed = med
+        if med == 2*dm:
+            tmpMed = 2*dm - 5
+#        print tmpMed, dm
+        dirname = name+'_MSC'+str(tmpMed)+'_MDM'+str(dm)
+        print dirname
+        command = 'grep -a pb '+dirname+'/Events/run_01/run_01_tag_1_banner.txt | awk \'{print "'+str(tmpMed)+' '+str(dm)+'",$6}\' >> '+filename
+        print command
+        os.system(command)
