f695e39ea8b532d54b8333853e4c8ed1e6b6c042
Shin-Shan <syu@cern.ch>
2016-11-30 10:38:29 +0100

    getting cross section for 2HDM model

---
+++ launch_scripts/getCrossSection_2HDM.py
@@ -0,0 +1,19 @@
+import os
+import sys
+print sys.argv
+
+name=sys.argv[1]
+filename='crossSection_'+name+'.txt'
+os.system('rm -rf '+filename)
+
+dmrange =[1]
+med2range = [300,400,500,600,700,800]
+medrange=[600,800,1000,1200,1400,1700,2000,2500]
+for med in medrange:
+  for med2 in med2range:							
+    for dm in dmrange:	
+        dirname = name+'_MZp'+str(med)+'_MA0'+str(med2)+'_MDM'+str(dm)
+        print dirname
+        command = 'grep -a pb '+dirname+'/Events/run_01/run_01_tag_1_banner.txt | awk \'{print "'+str(med)+' '+str(med2)+' '+str(dm)+'",$6}\' >> '+filename
+        print command
+        os.system(command)
