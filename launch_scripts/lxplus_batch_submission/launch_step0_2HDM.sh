aae3fb4051f13dab0a444cdc4f705b58b64cc8fe
Shin-Shan <syu@cern.ch>
2016-12-13 12:26:52 +0100

    adding scripts for 2HDM

---
+++ launch_scripts/lxplus_batch_submission/launch_step0_2HDM.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+scriptname=`basename $0`
+EXPECTED_ARGS=3
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo "Usage: $scriptname MZp MA0 mchi"
+    echo "Example: ./$scriptname 1000 300 10"
+    exit 1
+fi
+
+export PRODHOME=`pwd`
+name=2HDM
+
+mmed=$1
+mmed2=$2
+mchi=$3
+
+echo "Producing cards for Z' mass = "$mmed" GeV "
+echo "Producing cards for A0 mass = "$mmed2" GeV "
+echo "Producing cards for DM mass = "$mchi" GeV "
+echo ""
+newname=${name}\_MZp${mmed}\_MA0${mmed2}\_MDM${mchi}
+outputfile=step0_${newname}.root
+bsub -q1nd -R "rusage[mem=12000]" $PWD/runStep0.sh $PWD $newname $outputfile $name
+
+
+
+
+
